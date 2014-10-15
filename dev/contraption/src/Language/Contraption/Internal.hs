ccaf43f265cf605a553af89681486945ef8c6c41
Scott Murphy <scottmurphy09@gmail.com>
2014-10-15 13:00:10 -0500

    still toying

--- dev/contraption/src/Language/Contraption/Internal.hs
+++ dev/contraption/src/Language/Contraption/Internal.hs
@@ -1,3 +1,29 @@
+
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE PolyKinds                 #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeOperators             #-}
+{-# LANGUAGE TypeSynonymInstances      #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
 module Language.Contraption.Internal
     (
+      mkSemantics
     ) where
+
+import           BasicPrelude
+import           Data.Vinyl.TH
+
+mkSemantics n s = semantics n (map semBuilder s)
+  where
+     semBuilder (c, t) = c :~> t
+
+
+
